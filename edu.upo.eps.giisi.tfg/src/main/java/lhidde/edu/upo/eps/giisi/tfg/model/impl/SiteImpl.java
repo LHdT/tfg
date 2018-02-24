c6856e27bb54286dc4d3f5e39d92aa3417c80d2b
Luis Hidalgo <luishidalgodetena@gmail.com>
2018-02-24 12:26:10 +0100

    fix

--- edu.upo.eps.giisi.tfg/src/main/java/lhidde/edu/upo/eps/giisi/tfg/model/impl/SiteImpl.java
+++ edu.upo.eps.giisi.tfg/src/main/java/lhidde/edu/upo/eps/giisi/tfg/model/impl/SiteImpl.java
@@ -9,16 +9,19 @@ import org.jsoup.nodes.Document;
 
 import lhidde.edu.upo.eps.giisi.tfg.model.Site;
 
-public abstract class SiteImpl<T> implements Site{
+public abstract class SiteImpl<T,K,V> implements Site {
     protected String url;
     protected List<T> ingredients;
     protected Document root;
-    protected Map<T, T> values;
+    protected Map<K, V> values;
     protected static final String DOMAIN = "";
-/**
- * Generic constructor
- * @param url
- */
+
+    protected SiteImpl() {}
+    /**
+     * Generic constructor
+     * 
+     * @param url
+     */
     public SiteImpl(String url) {
 	if (!url.contains(DOMAIN))
 	    throw new RuntimeException("not compatible URL");
@@ -35,57 +38,47 @@ public abstract class SiteImpl<T> implements Site{
 	    e.printStackTrace();
 	}
     }
-    
-    
-
     public List<T> getIngredients() {
-    return ingredients;
-}
+	return ingredients;
+    }
 
+    public void setIngredients(List<T> ingredients) {
+	this.ingredients = ingredients;
+    }
 
+    public Map<K, V> getValues() {
+	return values;
+    }
 
-public void setIngredients(List<T> ingredients) {
-    this.ingredients = ingredients;
-}
-
-
-
-public Map<T, T> getValues() {
-    return values;
-}
-
-
-
-
-
-
-
-public String getDomain() {
-    return DOMAIN;
-}
-
-
+    public String getDomain() {
+	return DOMAIN;
+    }
 
     protected void loadNutritionalData() {
 	throw new RuntimeException("unimplemented method");
-	
+
     }
 
     protected void loadIngredients() {
 	throw new RuntimeException("unimplemented method");
-	
+
     }
 
     private void snifContent() throws IOException {
 	root = Jsoup.connect(url).get();
 
     }
+
     public String toString() {
+
+	//TODO: it should evolve to standard Json
+	return "{"
+		+ "ingredients :" + ingredients 
+		+ ",nutritionalValues:" + values
+	+ "}";
+
 	
-	String str;
-	
-	str=ingredients+"\n"+values;
-	return str;
+
     }
 
 }
