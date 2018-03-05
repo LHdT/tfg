0840d8722a63ed48f929e2adcc1f5b3614107533
Luis Hidalgo <luishidalgodetena@gmail.com>
2018-03-05 21:19:03 +0100

    add parent pom

---
+++ src/main/java/edu/upo/lhidde/TFG/Multimoudule_PoC/App.java
@@ -0,0 +1,13 @@
+package edu.upo.lhidde.TFG.Multimoudule_PoC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
